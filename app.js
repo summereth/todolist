667c3e8c75b976dc210c2f8f5a1bb16e041d05b9
Summer <liq.qian.l@gmail.com>
2023-08-07 14:44:47 +0800

    update port and resolve the bug of listen

--- app.js
+++ app.js
@@ -123,9 +123,8 @@ app.get("/about", (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-app.listen(port);
 
 
 app.listen(port, () => {
